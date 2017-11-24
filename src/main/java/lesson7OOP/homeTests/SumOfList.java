e9c141b8d8029a8b83ae4ce5fc2e078773015a0e
Kreeck <Kreeck@mail.ua>
2017-11-24 03:44:48 +0200

    7 lessons OOP.(String "0.00001" to double).

--- src/main/java/lesson7OOP/homeTests/SumOfList.java
+++ src/main/java/lesson7OOP/homeTests/SumOfList.java
@@ -7,15 +7,21 @@ public class SumOfList {
 
     public static void main(String[] args) {
         final List<String> doubleValues = new ArrayList<>();
-        double value = 0;
-        doubleValues.add("0.0001");
-        doubleValues.add("0.0002");
-        doubleValues.add("0.0034");
-        doubleValues.add("0.0002");
+        double value = 0.0;
+        doubleValues.add("5.01");
+        doubleValues.add("0.002");
+        doubleValues.add("0.00024");
+        doubleValues.add("0.000022");
 
         for (String elem : doubleValues) {
-            value += Double.parseDouble(elem);
+            value += Double.valueOf(elem);
         }
-        System.out.println(value);
+        System.out.printf("%.10f", value); // Formatting in print
+        System.out.println();
+
+        System.out.println(String.format("%.10f", value)); // Print with String formatting
+
+        double v = Double.parseDouble(String.valueOf(value)); //convert decimal number to double for print or calc
+        System.out.println(v);
     }
 }
